0c0381b51df3e5fc79355b8b2b1e0b49c15c255f
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-07-30 17:38:24 -0300

    seeds

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :filial
 
-  CATEGORIES = %w[Limpeza Comida Outro]
+  CATEGORIES = ["Limpeza e Higiene",
+  "Alimentação e Bebidas",
+  "Utensílios de Cozinha",
+  "Cama e Banho",
+  "Mobiliário e Decoração",
+  "Manutenção e Reparos",
+  "Conforto e Lazer",
+  "Segurança",
+  "Tecnologia e Eletrônicos",
+  "Papelaria e Escritório",
+  "Produtos de Hospitalidade",
+  "Roupas e Uniformes"]
 
   validates :name, presence: true
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,8 @@ Filial.destroy_all
 Item.destroy_all
 
 # Create filials
-filial1 = Filial.create!(name: 'Filial 1')
-filial2 = Filial.create!(name: 'Filial 2')
-
-# Create Items
-Item.create(name: "Detergente", quantity: 10, category: 'Limpeza', filial: filial1)
-Item.create(name: "Soap Powder", quantity: 2, category: 'Limpeza', filial: filial2)
-Item.create(name: "Rice Bag", quantity: 4, category: 'Comida', filial: filial1)
+filial1 = Filial.create!(name: 'Serra da Mantiqueira')
+filial2 = Filial.create!(name: 'Fattoria di Brauna')
 
 # Create an admin user
 admin = User.create!(
@@ -25,28 +20,19 @@ puts "Admin created: #{admin.email}"
 
 # Create manager users
 manager1 = User.create!(
-  email: 'manager1@example.com',
-  password: 'password',
-  password_confirmation: 'password',
+  email: 'estoque@villaggio.com',
+  password: 'Estoque10!',
+  password_confirmation: 'Estoque10!',
   role: :manager,
   filial: filial1
 )
 puts "Manager created: #{manager1.email}"
 
 manager2 = User.create!(
-  email: 'manager2@example.com',
-  password: 'password',
-  password_confirmation: 'password',
+  email: 'estoque2@gmail.com',
+  password: 'Estoque10!',
+  password_confirmation: 'Estoque10!',
   role: :manager,
   filial: filial2
 )
 puts "Manager created: #{manager2.email}"
-
-manager3 = User.create!(
-  email: 'manager3@example.com',
-  password: 'password',
-  password_confirmation: 'password',
-  role: :manager,
-  filial: filial1
-)
-puts "Manager created: #{manager3.email}"
