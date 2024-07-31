53bcd8275aa568d7cf3f1f47d2d5251c849f3061
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-07-31 10:58:44 -0300

    Add 2 filial mercadinho

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,53 @@
 # db/seeds.rb
 
-# Clear existing data
-User.destroy_all
-Filial.destroy_all
-Item.destroy_all
+# # Create filials
+# filial1 = Filial.create!(name: 'Serra da Mantiqueira')
+# filial2 = Filial.create!(name: 'Fattoria di Brauna')
 
-# Create filials
-filial1 = Filial.create!(name: 'Serra da Mantiqueira')
-filial2 = Filial.create!(name: 'Fattoria di Brauna')
+# # Create an admin user
+# admin = User.create!(
+#   email: 'villaggiogirotto@gmail.com',
+#   password: 'Aleserver10!',
+#   password_confirmation: 'Aleserver10!',
+#   role: :admin
+# )
+# puts "Admin created: #{admin.email}"
 
-# Create an admin user
-admin = User.create!(
-  email: 'villaggiogirotto@gmail.com',
-  password: 'Aleserver10!',
-  password_confirmation: 'Aleserver10!',
-  role: :admin
-)
-puts "Admin created: #{admin.email}"
+# # Create manager users
+# manager1 = User.create!(
+#   email: 'estoque@villaggio.com',
+#   password: 'Estoque10!',
+#   password_confirmation: 'Estoque10!',
+#   role: :manager,
+#   filial: filial1
+# )
+# puts "Manager created: #{manager1.email}"
 
-# Create manager users
-manager1 = User.create!(
-  email: 'estoque@villaggio.com',
+# manager2 = User.create!(
+#   email: 'estoque2@gmail.com',
+#   password: 'Estoque10!',
+#   password_confirmation: 'Estoque10!',
+#   role: :manager,
+#   filial: filial2
+# )
+# puts "Manager created: #{manager2.email}"
+
+
+mercadomg = Filial.create!(name: 'Mercadinho MG')
+mercadosp = Filial.create!(name: 'Mercadinho SP')
+
+manager3 = User.create!(
+  email: 'estoque3@gmail.com',
   password: 'Estoque10!',
   password_confirmation: 'Estoque10!',
   role: :manager,
-  filial: filial1
+  filial: mercadomg
 )
-puts "Manager created: #{manager1.email}"
 
-manager2 = User.create!(
-  email: 'estoque2@gmail.com',
+manager4 = User.create!(
+  email: 'estoque4@gmail.com',
   password: 'Estoque10!',
   password_confirmation: 'Estoque10!',
   role: :manager,
-  filial: filial2
+  filial: mercadosp
 )
-puts "Manager created: #{manager2.email}"
