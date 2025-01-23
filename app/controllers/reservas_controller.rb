9e4f29d51378a7b317504c4122bf55ef5f2a5d9d
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2025-01-23 20:06:29 -0300

    fix - authenticate new reserva

--- app/controllers/reservas_controller.rb
+++ app/controllers/reservas_controller.rb
@@ -5,6 +5,7 @@ class ReservasController < ApplicationController
   layout "clientside"
   before_action :set_reserva, only: [:show, :pay]
   skip_before_action :verify_authenticity_token, only: [:payment_webhook]
+  skip_before_action :authenticate_user!
 
   def index
     @reservas = current_user.reservas.order(:start_date)
