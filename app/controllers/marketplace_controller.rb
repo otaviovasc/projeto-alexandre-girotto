75923124a4bf21774148a8334ff0b1adbe570b6f
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2025-02-04 20:27:14 -0300

    fix - ver marketplace

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -1,6 +1,5 @@
 class MarketplaceController < ApplicationController
   layout "clientside"
-  before_action :check_active_reserva
 
   def services
     @services = Service.where(filial_id: @reserva.cabana.filial_id)
@@ -21,7 +20,7 @@ class MarketplaceController < ApplicationController
   private
 
   def check_active_reserva
-    @reserva = current_user.reservas.find_by('start_date <= ? AND end_date >= ?', Date.today, Date.today)
+    @reserva = current_user.reservas.find_by(payment_status: 'paid')
     unless @reserva
       redirect_to root_path, alert: 'Você precisa de uma reserva ativa para acessar a loja.'
     end
