52eda9787b1536d0e41a1a61c374137496a26da9
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2025-02-10 11:07:50 -0300

    feat - redirect new fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  def after_sign_in_path_for(resource)
-    restore_reserva_path || session.delete(:return_to) || root_path
-  end
-
-  def after_sign_up_path_for(resource)
-    restore_reserva_path || session.delete(:return_to) || root_path
-  end
+  before_action :store_user_location!, if: :should_store_location?
 
   private
 
-  def restore_reserva_path
-    if session[:reserva_params].present?
-      cabana_id = session[:cabana_id]
-      new_cabana_reserva_path(cabana_id: cabana_id)
-    end
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def should_store_location?
+    request.get? &&
+      !request.xhr? && # Evita chamadas AJAX
+      !request.path.match?(/\/calculate_price|\/unavailable_dates/) && # Evita salvar URLs de AJAX
+      !devise_controller? &&
+      !request.path.in?([new_user_registration_path, new_user_session_path])
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
   end
 end
