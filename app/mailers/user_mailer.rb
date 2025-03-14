5a77bb8726249411099ad5f4ec01a01dd1eb84f6
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2025-03-14 11:52:45 -0300

    chore: fix typo nos emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @user = user
     @password = generated_password
     @url  = 'https://www.villaggiogirotto.com.br/users/sign_in'
-    mail(to: @user.email, subject: 'Sua hospedagem - Villagio')
+    mail(to: @user.email, subject: 'Sua hospedagem - Villaggio')
   end
 
   def reserva_created(user, reserva)
@@ -13,7 +13,7 @@ class UserMailer < ApplicationMailer
     @reserva = reserva
     @url  = "https://www.villaggiogirotto.com.br/reservas/#{reserva.id}"
     @services = reserva.services.includes(:reserva_services)
-    mail(to: @user.email, subject: 'Aguardando Pagamento - Villagio')
+    mail(to: @user.email, subject: 'Aguardando Pagamento - Villaggio')
   end
 
   def reserva_paid(user, reserva)
@@ -21,13 +21,13 @@ class UserMailer < ApplicationMailer
     @reserva = reserva
     @url  = "https://www.villaggiogirotto.com.br/reservas/#{reserva.id}"
     @services = reserva.services.includes(:reserva_services)
-    mail(to: @user.email, subject: 'Pagamento Confirmado - Villagio')
+    mail(to: @user.email, subject: 'Pagamento Confirmado - Villaggio')
   end
 
   def notify_adm(user, reserva)
     @user = user
     @reserva = reserva
     @url  = "https://www.villaggiogirotto.com.br/admin/reservas_summary"
-    mail(to: 'contato@villaggiogirotto.com.br', subject: "Reserva: #{reserva.payment_status} - Villagio")
+    mail(to: 'contato@villaggiogirotto.com.br', subject: "Reserva: #{reserva.payment_status} - Villaggio")
   end
 end
