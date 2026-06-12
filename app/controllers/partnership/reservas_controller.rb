8247d621f69cf7b4004bd3a2777f8511a37cee34
Villaggio Girotto <villaggiogirotto@Villaggios-MacBook-Pro.local>
2026-06-12 15:59:27 -0300

    Permite selecionar hospede em parcerias

--- app/controllers/partnership/reservas_controller.rb
+++ app/controllers/partnership/reservas_controller.rb
@@ -2,26 +2,19 @@ class Partnership::ReservasController < ApplicationController
   before_action :authorize_partnership_access
 
   def new
-    @partnership_form = true
+    setup_form
     @reserva = Reserva.new(observation: 'Parceria')
-    @services = Service.all
 
     render template: 'admin/reservas/new'
   end
 
   def create
-    @user = find_or_initialize_guest
-    if @user.persisted? && !@user.client?
-      render_new_with_error('Este e-mail já pertence a um usuário interno do sistema. Use outro e-mail para o hóspede.')
-      return
-    end
+    @user = params[:create_user] == 'true' ? create_or_update_guest : selected_existing_guest
+    return unless @user
 
-    unless @user.save
-      render_new_with_error("Não foi possível salvar o hóspede. Verifique os dados informados: #{@user.errors.full_messages.join(', ')}")
-      return
-    end
+    @user.update_column(:partner, true) unless @user.partner?
 
-    @reserva = Reserva.new(partnership_reserva_params.merge(
+    @reserva = Reserva.new(partnership_reserva_params.except(:user_id).merge(
       user: @user,
       payment_status: 'paid',
       observation: partnership_reserva_params[:observation].presence || 'Parceria',
@@ -46,6 +39,29 @@ class Partnership::ReservasController < ApplicationController
     redirect_to root_path, alert: 'Você não tem permissão para criar reservas de parceria.'
   end
 
+  def create_or_update_guest
+    user = find_or_initialize_guest
+    if user.persisted? && !user.client?
+      render_new_with_error('Este e-mail já pertence a um usuário interno do sistema. Use outro e-mail para o hóspede.')
+      return
+    end
+
+    unless user.save
+      render_new_with_error("Não foi possível salvar o hóspede. Verifique os dados informados: #{user.errors.full_messages.join(', ')}")
+      return
+    end
+
+    user
+  end
+
+  def selected_existing_guest
+    user = User.clients.find_by(id: partnership_reserva_params[:user_id])
+    return user if user
+
+    render_new_with_error('Selecione um hóspede existente ou marque criar novo hóspede.')
+    nil
+  end
+
   def find_or_initialize_guest
     user = User.find_or_initialize_by(email: partnership_user_params[:email])
     generated_password = SecureRandom.alphanumeric(12)
@@ -57,10 +73,17 @@ class Partnership::ReservasController < ApplicationController
     user
   end
 
-  def render_new_with_error(message)
+  def setup_form
     @partnership_form = true
-    @reserva ||= Reserva.new(observation: 'Parceria')
+    @creating_new_guest = false if @creating_new_guest.nil?
+    @existing_users = User.clients.order(:name, :email)
     @services = Service.all
+  end
+
+  def render_new_with_error(message)
+    setup_form
+    @creating_new_guest = params[:create_user] == 'true'
+    @reserva ||= Reserva.new(observation: 'Parceria')
     flash.now[:alert] = message
     render template: 'admin/reservas/new', status: :unprocessable_entity
   end
@@ -72,6 +95,7 @@ class Partnership::ReservasController < ApplicationController
       :end_date,
       :total_price,
       :observation,
+      :user_id,
       reserva_services_attributes: [:id, :service_id, :quantity, :service_date, :status, :observation, :_destroy]
     )
   end
