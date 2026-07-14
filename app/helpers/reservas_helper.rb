fb7842df6155197f92141a7df06dec7cbc6a8c95
Villaggio Girotto <villaggiogirotto@Villaggios-MacBook-Pro.local>
2026-07-14 14:41:21 -0300

    Centraliza codigo da reserva no calendario

--- app/helpers/reservas_helper.rb
+++ app/helpers/reservas_helper.rb
@@ -50,4 +50,11 @@ module ReservasHelper
 
     segments
   end
+
+  def calendar_reservation_code_date?(reserva, date)
+    return false if reserva.start_date.blank? || reserva.end_date.blank?
+
+    midpoint_offset = ((reserva.end_date - reserva.start_date).to_i / 2.0).floor
+    date == reserva.start_date + midpoint_offset.days
+  end
 end
