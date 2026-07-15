3dafa9ef188e763423a8a602cf580d11df273265
Villaggio Girotto <villaggiogirotto@Villaggios-MacBook-Pro.local>
2026-07-14 22:13:03 -0300

    Confirma pagamento ao retornar da Cielo

--- app/controllers/reserva_payments_controller.rb
+++ app/controllers/reserva_payments_controller.rb
@@ -6,6 +6,9 @@ class ReservaPaymentsController < ApplicationController
 
   def show
     @reserva = @reserva_payment.reserva
+    sync_cielo_checkout_status!
+    @reserva_payment.reload
+    @reserva = @reserva_payment.reserva
   end
 
   def accept_terms
@@ -38,5 +41,39 @@ class ReservaPaymentsController < ApplicationController
 
   def set_reserva_payment
     @reserva_payment = ReservaPayment.includes(reserva: [:user, { cabana: :filial }]).find_by!(terms_token: params[:token])
+  end
+
+  def sync_cielo_checkout_status!
+    return unless @reserva_payment.waiting_payment?
+    return if @reserva_payment.payment_order_code.blank?
+
+    filial = @reserva_payment.reserva.cabana.filial
+    transaction = CieloCheckoutService::TransactionQuery.new(
+      client_id: filial.cielo_checkout_client_id_for_payments,
+      client_secret: filial.cielo_checkout_client_secret_for_payments
+    ).find_by_order_number(@reserva_payment.payment_order_code)
+
+    status = CieloCheckoutService.payment_status_from_transaction(transaction)
+    return if status.blank?
+
+    checkout_order_number = transaction['checkoutOrderNumber'].presence || @reserva_payment.payment_link_id
+    remember_cielo_checkout_order_number(checkout_order_number)
+
+    PaymentStatusProcessor.call(
+      identifiers: [@reserva_payment.payment_order_code, @reserva_payment.payment_link_id, checkout_order_number],
+      status: status
+    )
+  rescue CieloCheckoutService::Error => e
+    Rails.logger.warn("Unable to sync Cielo Checkout reservation payment status: #{e.message}")
+  end
+
+  def remember_cielo_checkout_order_number(checkout_order_number)
+    return if checkout_order_number.blank?
+    return if @reserva_payment.payment_link_id == checkout_order_number
+
+    @reserva_payment.update_columns(
+      payment_link_id: checkout_order_number,
+      updated_at: Time.current
+    )
   end
 end
